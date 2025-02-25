db7c694de28c12fe2d7cf93ab632ba5c79fc445c
jimwell <jimwelllmanalo@gmail.com>
2025-02-26 01:30:58 +0800

    Add index route to pass messages dynamically to template

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
 
+app.use("/", indexRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
